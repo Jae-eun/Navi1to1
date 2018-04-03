369ea43d0ac0fac932b3b695bc5b418fe2e29541
hyeseh9418 <32292648+hyeseh9418@users.noreply.github.com>
2018-04-03 17:35:27 +0900

    1st commit
    complete

---
+++ Navigation1to1/OrderViewController.swift
@@ -0,0 +1,40 @@
+//
+//  OrderViewController.swift
+//  Navigation1to1
+//
+//  Created by SWUCOMPUTER on 2018. 4. 3..
+//  Copyright © 2018년 SWUCOMPUTER. All rights reserved.
+//
+
+import UIKit
+
+class OrderViewController: UIViewController {
+    
+    @IBOutlet var infoLabel: UILabel!
+    var info: String?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if let contentString = info {
+            infoLabel.text = contentString
+            
+        // Do any additional setup after loading the view.
+    }
+
+
+
+
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
